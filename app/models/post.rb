5b9e77cae45d47596fb0c035c017fe59999aa29f
Shinonononon <h1karu.sn0928@gmail.com>
2024-06-06 14:48:38 +0900

    first

---
+++ app/models/post.rb
@@ -0,0 +1,10 @@
+class Post < ApplicationRecord
+    validates :title, length: { in: 1..20,
+    too_long:'20字以内で入力してください',
+    too_short:'何かしら入力してください'
+}
+    validates :content, length: { in: 1..140,
+    too_long:'140文字以内で入力してください。',
+    too_short:'何かしら入力してください。'
+    }
+end
